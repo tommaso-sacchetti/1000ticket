7e4a2f0ac76eaeefd61855abbf733f354167d59b
tommaso-sacchetti <tommaso.sacchetti97@gmail.com>
2019-05-23 11:12:58 +0200

    started view project

---
+++ View/VenditeWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace View
+{
+    /// <summary>
+    /// Logica di interazione per VenditeWindow.xaml
+    /// </summary>
+    public partial class VenditeWindow : Window
+    {
+        public VenditeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+    }
+}
